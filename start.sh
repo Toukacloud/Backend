02b0dbcc9a332e69fb0768e749ee9c9cf1ad7e0f
Lylia <71930916+dishapatel010@users.noreply.github.com>
2022-07-02 19:12:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+cd /usr/src/app
+
+VERSION="latest"
+
+REPO="dishapatel010/Dester-Release"
+
+curl -L -s $(curl -s "https://api.github.com/repos/${REPO}/releases/${VERSION}" | grep -Po '"browser_download_url": "\K.*?(?=")') | tar xf - -C .
+
+pip3 install -r requirements.txt -q --no-cache-dir
+
 uvicorn main:app --host 0.0.0.0 --port=$PORT
